79b0a7168e6d8a293593ac47e3cd3c28dd7d3085
cowbon <alexalex850520@gmail.com>
2019-02-28 22:04:13 +0800

    init client app, move flask to server folder

--- server/app.py
+++ server/app.py
@@ -69,9 +69,3 @@ def handle_message(event):
         )
     )
     line_bot_api.push_message(user_id, buttons_template_message)
-
-
-import os
-if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
