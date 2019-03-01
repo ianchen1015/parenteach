a9511e4d47034a9373d5cfa0a19f3da3e14efd5d
ian <ianchen1015@gmail.com>
2019-03-01 20:30:38 +0800

    show receive event

--- server/app.py
+++ server/app.py
@@ -40,46 +40,23 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     #print("Handle: reply_token: " + event.reply_token + ", message: " + event.message.text)
-    content = "{}: {}".format(event.source.user_id, event.message.text)
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=content))
+    # content = "{}: {}".format(event.source.user_id, event.message.text)
+    # line_bot_api.reply_message(
+    #     event.reply_token,
+    #     TextSendMessage(text=content))
 
     user_id = event.source.user_id
     profile = line_bot_api.get_profile(user_id)
 
-    line_bot_api.push_message(user_id, TextSendMessage(text=profile.display_name+profile.user_id+profile.picture_url+profile.status_message))
+    print(event)
 
-    buttons_template_message = TemplateSendMessage(
-        alt_text='Buttons template',
-        template=ButtonsTemplate(
-            thumbnail_image_url='https://example.com/image.jpg',
-            title='Menu',
-            text='Please select',
-            actions=[
-                PostbackAction(
-                    label='postback',
-                    text='postback text',
-                    data='action=buy&itemid=1'
-                ),
-                MessageAction(
-                    label='message',
-                    text='message text'
-                ),
-                URIAction(
-                    label='uri',
-                    uri='http://example.com/'
-                )
-            ]
-        )
-    )
-    line_bot_api.push_message(user_id, buttons_template_message)
+    line_bot_api.push_message(user_id, TextSendMessage(text=profile.display_name+profile.user_id+profile.picture_url+profile.status_message))
 
 @app.route("/absent", methods=['GET'])
 def absent():
 
     buttons_template_message = TemplateSendMessage(
-        alt_text='Buttons template',
+        alt_text='您的孩子尚未到校',
         template=ButtonsTemplate(
             text='⚠️ 尚未到校\n今日 (1/03) 您的孩子 陳小明 尚未抵達教室，請問他是否需要請假呢？',
             actions=[
