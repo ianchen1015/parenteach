1075017223296343c0b315ae0bfbc86a273d9312
ian <ianchen1015@gmail.com>
2019-03-01 19:19:44 +0800

    show user message text

--- server/app.py
+++ server/app.py
@@ -23,7 +23,7 @@ def callback():
     # get request body as text
     body = request.get_data(as_text=True)
     print("Request body: " + body, "Signature: " + signature)
-    print(body['events'][0]['message']['text'])
+    print(body['events'][0])
 
     # handle webhook body
     try:
